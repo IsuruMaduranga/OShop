fc16bd9eab482343f1b12a6a19b67310dd917da8
Isuru Maduranga <isurumaduranga.official@gmail.com>
2018-07-20 21:42:55 +0530

    Added filtering for products page

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,33 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit,OnDestroy {
+  filteredProducts: {title: string}[];
+  products: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$=productService.getAll();
-   }
+    this.productService.getAll()
+    .subscribe(products => this.filteredProducts=this.products=products);
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+  filter(query: string){
+    this.filteredProducts = (query)?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
   }
 
 }
